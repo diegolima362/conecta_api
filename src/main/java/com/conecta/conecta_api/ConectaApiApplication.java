d22cb907a491318315ef0097d767f853dddcf2bd
diegolima362 <diegolima362@gmail.com>
2022-05-20 19:23:58 -0300

    initial commit

---
+++ src/main/java/com/conecta/conecta_api/ConectaApiApplication.java
@@ -0,0 +1,13 @@
+package com.conecta.conecta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectaApiApplication.class, args);
+    }
+
+}
